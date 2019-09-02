600cf2a3fb2ede8226d2df6adabe2ac52bcd3310
Luke Brown <me@luke.sx>
2019-09-02 13:03:33 +0100

    SongCreate, SongList, fetchSongs

---
+++ Lyrical-GraphQL/client/component/SongList.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import gql from 'graphql-tag';
+import {graphql} from 'react-apollo';
+import {Link} from 'react-router';
+import query from '../queries/fetchSongs';
+
+// works
+class SongList extends Component {
+	renderSongs() {
+		console.log(this.props);
+		return this.props.data.songs.map(song => {
+			return (
+				<li key={song.id} className="collection-item">
+					{song.title}
+				</li>
+			)
+		})
+
+	}
+
+	render() {
+		console.log(this.props);
+
+
+		if (this.props.data.loading) {
+			return <div>Loading...</div>;
+		}
+		return (
+			<div>
+				<ul className="collection">
+					{this.renderSongs()}
+				</ul>
+
+				<Link to="/songs/new"
+				      className="btn-floating btn-large red right"
+				>
+					<i className="material-icons">add</i>
+				</Link>
+			</div>
+		)
+	}
+}
+
+const mutation = gql`
+	mutation DeleteSong($id: ID) {
+		deleteSong(id: $id) {
+			id
+		}
+	}
+`;
+
+export default graphql(muation)(
+	graphql(query)(SongList)
+);
--- Lyrical-GraphQL/client/index.js
+++ Lyrical-GraphQL/client/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute} from 'react-router';
+import ApolloClient from 'apollo-client';
+import {ApolloProvider} from 'react-apollo';
+import SongList from './component/SongList';
+import App from './component/App';
+import SongCreate from './component/songCreate';
+
+const client = new ApolloClient({});
 
 const Root = () => {
-  return <div>Lyrical</div>
+	return (
+		<ApolloProvider client={client}>
+			<Router history={hashHistory}>
+				<Route path="/" component={App}>
+					<IndexRoute component={SongList} />
+				</Route>
+				<Route path="/songs/new" component={SongCreate} />
+			</Router>
+		</ApolloProvider>
+	)
 };
 
 ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
+	<Root/>,
+	document.querySelector('#root')
 );
